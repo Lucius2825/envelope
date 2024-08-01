9239cf039f0744647b082326be74fb7ec180d73a
Khant Sett Win <wangftksw2825@gmail.com>
2024-08-02 00:44:39 +0630

    update

--- js/script.js
+++ js/script.js
@@ -5,7 +5,28 @@ $(document).ready(function() {
         }, "slow");
     }).mouseleave(function(){
         $(".card").stop().animate({
-            top:0
-        }, "slow");
+            top: 0
+        }, "slow", function() {
+            // Show the message and start the countdown
+            showMessageAndCountdown();
+        });
     });
+    
+    function showMessageAndCountdown() {
+        $("#messageBox").removeClass("hidden");
+        
+        var countdown = 5;  // Start countdown from 5
+        var countdownElement = $("#countdown");
+        countdownElement.text(countdown);
+        
+        var interval = setInterval(function() {
+            countdown--;
+            if (countdown >= 1) {
+                countdownElement.text(countdown);
+            } else {
+                clearInterval(interval);
+                window.location.href = "https://github.com/FIQTOR/flowers-for-someone.git"; // Redirect to another page
+            }
+        }, 1000);
+    }
 });
